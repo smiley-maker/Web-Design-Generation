0f0a3dddfc81ea92cc4ea16cfde21964894e52e3
Jordan Sinclair <jordan.sinclair@du.edu>
2022-11-01 05:40:25 -0600

    Added chromosone file

---
+++ chromosone.py
@@ -0,0 +1,8 @@
+import gene
+
+class chromosone:
+    def __init__(self, vals={}):
+        self.genes = {
+            "elementwidth": gene(["1/4", "1/2", "1"], vals.elementwidth),
+            
+        }
