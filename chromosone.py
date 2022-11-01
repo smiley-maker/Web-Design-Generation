5cd02462991b13d60bbe05d6d65dbd92199a1cb1
Jordan Sinclair <jordan.sinclair@du.edu>
2022-11-01 05:52:16 -0600

    Updated chromosone with object rep method

--- chromosone.py
+++ chromosone.py
@@ -3,6 +3,10 @@ import gene
 class chromosone:
     def __init__(self, vals={}):
         self.genes = {
-            "elementwidth": gene(["1/4", "1/2", "1"], vals.elementwidth),
-            
+            "elementwidth": gene(["1/4", "1/2", "1"], vals.elementwidth),   
         }
+    def getObjectRepresentation(self):
+        obj = {}
+        for name in self.genes.keys:
+            obj[name] = name.getValue()
+        return obj
