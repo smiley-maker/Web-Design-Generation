7b58bb0db1b63792876a0c69145cdac45ab8e1d7
Jordan Sinclair <jordan.sinclair@du.edu>
2022-11-01 06:34:34 -0600

    Updaed chromosone file

--- chromosone.py
+++ chromosone.py
@@ -1,4 +1,7 @@
+import math
 import gene
+import random
+from functools import reduce
 
 class chromosone:
     def __init__(self, vals={}):
@@ -10,3 +13,30 @@ class chromosone:
         for name in self.genes.keys:
             obj[name] = name.getValue()
         return obj
+    @staticmethod
+    def uniformCrossover(self, cr1, cr2, p=0.5):
+        newProps = {}
+        for name in cr1.genes.keys:
+            if random.random() < p:
+                newProps[name] = cr1.genes[name].getValue()
+            else:
+                newProps[name] = cr2.genes[name].getValue()
+        return chromosone(newProps)
+    def mutate(self, p = 0.02) :
+        newChromosome = chromosone(self.getObjectRepresentation())
+        for name in newChromosome.genes.keys:
+            newChromosome.genes[name].mutate(p)
+        return newChromosome
+    def selection(self, pop, num):
+        fitnesses = list(map(pop, lambda obj: obj.fitness))
+        totalFitness = list(reduce((lambda tot, current: tot + current), fitnesses))
+        indices = []
+        while num:
+            num -= 1;
+            randVal = math.floor(random.random()*totalFitness)
+            for idx in range(len(fitnesses)):
+                randVal -= fitnesses[idx]
+                if randVal <= 0:
+                    indices.push(idx)
+                    break
+        return indices
